7f254e685df58f2025447ea96359f5bbb12ac214
ryuichi24 <ryuichi.nishi24@gmail.com>
2025-05-28 00:32:17 +0200

    feat: add keyx project

---
+++ apps/keyx/scripts/release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+shopt -s xpg_echo
+
+if [ -d "build" ]; then
+    rm -rf build
+fi
+
+PROJECT_NAME="keyx"
+
+echo "\nBuilding $PROJECT_NAME...\n"
+
+# -S: Source directory, -B: Build directory
+cmake -S . -B build
+
+make -C ./build
